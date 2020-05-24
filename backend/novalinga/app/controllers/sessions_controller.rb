4cea1339b70bd64d272570de8455c916888ffc0a
unknown <christianp1894gmail.com>
2020-05-24 17:58:40 -0500

    google oauth2 login logout

--- backend/novalinga/app/controllers/sessions_controller.rb
+++ backend/novalinga/app/controllers/sessions_controller.rb
@@ -2,22 +2,23 @@ class SessionsController < ApplicationController
   layout false
   def index
   end
-
   def create
+    user_info = request.env["omniauth.auth"]
     @auth = request.env['omniauth.auth']['credentials']
+    @auth["email"] = user_info["info"]["email"]
+    @auth["image"] = user_info["info"]["image"]
+    @auth["uid"] = user_info["uid"]
     Token.create(
       access_token: @auth['token'],
       refresh_token: @auth['refresh_token'],
       expires_at: Time.at(@auth['expires_at']).to_datetime
       )
-      redirect_to root_path
   end
   def new
   end
 
   def destroy
-    session.delete :user
-  
+    Token.last.destroy  
     redirect_to root_path
   end
 end
